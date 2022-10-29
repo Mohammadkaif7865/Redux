34de2befe3ffc1a8f978ba247ca5acf4c734134a
mohammad kaif <mohammadkaif051197@gmail.com>
2022-10-29 07:49:23 +0530

    yes me

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-
+// * The state is a built-in React object that is used to contain data or information about the component. A component's state can change over time; whenever it changes, the component re-renders
+// * A store that holds the state of your application.
+// * An action that describes the changes in the state of the application.
+// * A reducer which actually carries out the state transition depending on the action.
 function App() {
   return (
     <div className="App">
