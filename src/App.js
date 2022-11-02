6c8a72d72ac407866acca969f80000b17bcdd5aa
mohammad kaif <mohammadkaif051197@gmail.com>
2022-11-02 19:35:17 +0530

    yes me

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
     </div>
   );
 }
+// just chilling around
 // * The state is a built-in React object that is used to contain data or information about the component. A component's state can change over time; whenever it changes, the component re-renders
 
 // * A store that holds the state of your application.
