fe133ffd39906698c1fb15f743635e4f5d87ef5f
mohammad kaif <mohammadkaif051197@gmail.com>
2022-11-04 09:02:39 +0530

    yes ne

--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,28 @@
-import { createStore } from 'redux';
-const reducerfn = (state = { counter: 0 }, action) => {
-    // Synchronous Function
-    // We should not mutate the original state
-    if (action.type === "INC") {
-        return {
-            counter: state.counter + 1
-        }
-    }
-    if (action.type === "DEC") {
-        return {
-            counter: state.counter - 1
-        }
-    }
-    if (action.type === "ADD") {
-        return {
-            counter: state.counter + action.payload
-        }
-    }
-    return state;
-}
-const store = createStore(reducerfn);
-export default store;
+
+
+
+
+// ! This is a simple example of react redux
+// import { createStore } from 'redux';
+// const reducerfn = (state = { counter: 0 }, action) => {
+//     // Synchronous Function
+//     // We should not mutate the original state
+//     if (action.type === "INC") {
+//         return {
+//             counter: state.counter + 1
+//         }
+//     }
+//     if (action.type === "DEC") {
+//         return {
+//             counter: state.counter - 1
+//         }
+//     }
+//     if (action.type === "ADD") {
+//         return {
+//             counter: state.counter + action.payload
+//         }
+//     }
+//     return state;
+// }
+// const store = createStore(reducerfn);
+// export default store;
