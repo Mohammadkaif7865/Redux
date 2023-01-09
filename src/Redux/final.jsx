3036edc42c61e4ca3990f49a6b80db9708b2abfb
mohammad kaif <mohammadkaif051197@gmail.com>
2023-01-09 12:38:59 +0530

    this is the starting of mui course

--- src/Redux/final.jsx
+++ src/Redux/final.jsx
@@ -31,3 +31,8 @@ export const final = createSlice({
 });
 export const {addFinalText}  = final.actions;
 export default final.reducer;
+// ? Steps to create slice with createAsyncThunk to initiate the data with default value or initial value
+// * create a slice  and define its state , reducer and extra reducer (Here the promise will be resolved)
+// * create a function inside of slice or import it from outside 
+// * Import thunk function in the desired compoenent from whrere you want to dispatch it.
+// * And it is not neccessary to have the promise handler of thunk function in slice you can use it directly to call api but syntax is still unknown to me
