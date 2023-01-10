a34f07ce5b74941d3ae17f18d618d8b453efc604
mohammad kaif <mohammadkaif051197@gmail.com>
2023-01-10 13:39:08 +0530

    yes me

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
-import MuiButton from './components/MuiButton';
+import MuiTextField from './components/MuiTextField';
+// import MuiButton from './components/MuiButton';
 // import MuiTypography from './components/MuiTypography';
 function App() {
-  
+
   return (
-    
+
     <div className="App">
-      <MuiButton/>
-        {/* <MuiTypography/> */}
+      <MuiTextField />
+      {/* <MuiButton/> */}
+      {/* <MuiTypography/> */}
     </div>
   );
 }
---
+++ src/components/MuiTextField.jsx
@@ -0,0 +1,63 @@
+import { InputAdornment, Stack, TextField } from "@mui/material";
+import { useState } from "react";
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+function MuiTextField() {
+    const [visibilty, setVisibility] = useState("password");
+    const changePasswordVisibilty = () => {
+        if (visibilty === "password") {
+            setVisibility("text");
+        }
+        if (visibilty === "text") {
+            setVisibility("password");
+        }
+    }
+    return (
+        <>
+            <Stack spacing={4}>
+                <Stack spacing={2} direction="row">
+                    <TextField label="name" variant="outlined" color="secondary">
+                    </TextField>
+                    <TextField label="subject" variant="filled" color="error">
+                    </TextField>
+                    <TextField label="age" variant="standard" color="success">
+                    </TextField>
+                </Stack>
+                <Stack spacing={2} direction="row">
+                    <TextField label="
+                   Secondary small" variant="outlined" color="secondary" size="small">
+                    </TextField>
+                    <TextField label="
+                   Warning Large" variant="outlined" color="warning" size="large" required helperText="Do not give the wrong value">
+                    </TextField>
+                    <TextField label="Password" variant="outlined" color="secondary" size="medium" required helperText="Do not share your password with anyone" type="password" disabled>
+                    </TextField>
+                    <TextField label="Password" variant="outlined" value="dang" color="secondary" size="medium" required helperText="Do not share your password with anyone" InputProps={{ readOnly: true }}>
+                    </TextField>
+                </Stack>
+                <Stack direction="row" spacing={2}>
+                    <TextField
+                        label="Amount"
+                        InputProps={{
+                            startAdornment: <InputAdornment position="start">$</InputAdornment>
+                        }} />
+                    <TextField
+                        label="Weight"
+                        InputProps={{
+                            endAdornment: <InputAdornment position="end">kg</InputAdornment>
+                        }} />
+                    <TextField
+                        label="password"
+                        helperText="Enter your password"
+                        type={visibilty}
+                        InputProps={{
+                            endAdornment: <InputAdornment position="end">{
+                                visibilty === "passwprd" ? <VisibilityOffIcon onClick={changePasswordVisibilty} />
+                                    : <VisibilityIcon onClick={changePasswordVisibilty} />}</InputAdornment>
+                        }} />
+                </Stack>
+            </Stack>
+        </>
+    )
+}
+export default MuiTextField;
