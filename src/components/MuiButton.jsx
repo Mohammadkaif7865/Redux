e981244ede636635971359384e36193f1327ae18
mohammad kaif <mohammadkaif051197@gmail.com>
2023-01-10 12:41:23 +0530

    yes me

--- src/components/MuiButton.jsx
+++ src/components/MuiButton.jsx
@@ -1,6 +1,15 @@
-import { Button, Stack, IconButton , ButtonGroup} from "@mui/material";
+import { Button, Stack, IconButton, ButtonGroup, ToggleButton, ToggleButtonGroup } from "@mui/material";
+import FormatBoldIcon from "@mui/icons-material/FormatBold";
+import FormatUnderlined from "@mui/icons-material/FormatUnderlined";
+import FormatItalicIcon from "@mui/icons-material/FormatItalic";
 import SendIcon from "@mui/icons-material/Send";
+import { useState } from "react";
 function MuiButton() {
+    const [formats, setFormats] = useState([]);
+    console.log(formats);
+    const handleFormatChange = (event, updatedFormats) => {
+        setFormats(updatedFormats);
+    }
     return (
         <>
             <Stack spacing={4}>
@@ -10,7 +19,11 @@ function MuiButton() {
                     <Button variant="outlined">Outlined</Button>
                 </Stack>
                 <Stack spacing={2} direction="row">
-                  <Button variant="contained"></Button>
+                    <ButtonGroup aria-label="alignment button group" variant="contained" orientation="vertical" size='small' color="secondary">
+                        <Button onClick={() => alert("Left")}>Left</Button>
+                        <Button onClick={() => alert("Center")}>Center</Button>
+                        <Button onClick={() => alert("Right")}>Right</Button>
+                    </ButtonGroup>
                 </Stack>
                 <Stack spacing={2} direction="row">
                     <Button variant="contained" color="primary">Primary</Button>
@@ -41,11 +54,24 @@ function MuiButton() {
                 {/*Icon have 2000 icons options*/}
                 <Stack display="block" spacing={2} direction="row">
                     <Button variant="contained" startIcon={<SendIcon />}>Send</Button>
-                    <Button variant="contained" endIcon={<SendIcon />} disableElevation disableRipple value={2} onClick={(e) =>{ alert( e.target.value); console.log(e.target.value)}}>Send</Button>
+                    <Button variant="contained" endIcon={<SendIcon />} disableElevation disableRipple value={2} onClick={(e) => { alert(e.target.value); console.log(e.target.value) }}>Send</Button>
                     <Button variant="contained" endIcon={<SendIcon />}></Button>
                     <IconButton aria-label="send" color="warning" size="small">
                         <SendIcon />
                     </IconButton>
+                </Stack>
+                <Stack direction='row'>
+                    <ToggleButtonGroup aria-label="text formatting" value={formats} onChange={handleFormatChange} size="small" exclusive>
+                        <ToggleButton value="bold" aria-label="bold">
+                            <FormatBoldIcon />
+                        </ToggleButton>
+                        <ToggleButton value="italic" aria-label="italic">
+                            <FormatItalicIcon />
+                        </ToggleButton>
+                        <ToggleButton value="underlined" aria-label="underlined">
+                            <FormatUnderlined />
+                        </ToggleButton>
+                    </ToggleButtonGroup>
                 </Stack>
             </Stack>
         </>
